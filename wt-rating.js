cd0a4eecaea1938f28d6425cb526a70588ff9200
Maxi <maxicespedes7@hotmail.com>
2016-05-03 07:58:00 -0300

    find if the item doesnt exits

--- wt-rating.js
+++ wt-rating.js
@@ -1,48 +1,61 @@
-
+//Need to learn  JS >D
 var mongo = require('mongodb').MongoClient;
-
+var COLLECTION = 'items';
 var getNewAverage = function(quantity,currAvg,newPoint){
   console.log('Getting new average');
   return currAvg + ((newPoint-currAvg)/quantity);
 }
 
-var saveAvgToCollection = function(db,docQuery,newPoint,cb){
-    console.log('Getting Collection:' + docQuery);
-    //db.collection('items').findAndModify(docQuery,[],
-    db.collection('items').findAndModify({link:'putotototo'},[],
-                                         {
+var findItem = function(db,item,cb){
+  console.log(item);
+  db.collection(COLLECTION)
+    .findOne(item,function(err,doc){
+      if(err) throw err;
 
-                                           $avgPoints:getNewAverage(1,10,newPoint)
-                                         },
-                                         {upsert:true,
-                                          new:false},function(err,doc){
-                                                if (err) {
-                                                  console.log(err);
-                                                  throw err;
-                                                }
-                                                console.log(doc);
-                                                  cb(null,doc);
-                                              }
-  );
+      cb(null,doc);
+    });
+}
+
+var saveAvgToCollection = function(db,docQuery,newPoint,cb){
+   findItem(db,docQuery,function(err,findedDoc){
+      if (err) throw err;
+
+      if (!findedDoc) { //Insert
+        //Extend properties on the new item
+        docQuery.avgPoints=newPoint;
+        docQuery.quantity=1; //It can probably be setted automatically by the mongo engine.
+        console.log(docQuery);
+        db.collection(COLLECTION).insert(docQuery,function(err,res){
+          if(err) throw err;
+          cb(null,res);
+          return;
+        });
+      }else{          //Update
+        
+      }
+   });
 
 
 }
 
 module.exports = function(context,callback){
-  var link = context.data.link;
-  var points = context.data.points;
-  //console.log(context.data);
-  var colQuery = {link:context.data.link};
-  mongo.connect(context.data.MONGODB, function(err,db){
+  try {
+    var link = context.data.link;
+    var points = context.data.points;
+    var colQuery = {link:context.data.link};
 
-    if(err) return callback(err);
-    //console.log(colQuery);
-    saveAvgToCollection(db,colQuery,points,function(err,newAvg){
-      if (err) callback(err)
-      else
-        callback(null,newAvg);
-    });
-    //var newAvg=  db;
+    mongo.connect(context.data.MONGODB, function(err,db){
+      if(err) return callback(err);
+      saveAvgToCollection(db,colQuery,points,function(err,itemSaved){
+        if (err) throw err;
+        console.log('FROM MAIN CALLBACK');
+        callback(null,itemSaved);
+      });
 
-  });
+
+      });
+  } catch (e) {
+    callback('Exiting with error: '+e,null);
+  }
+
 }
