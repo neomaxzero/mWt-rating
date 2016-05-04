09548cadc13d86dc005a1e3fae1730a6967d61bb
unknown <mcespedes@mcespedes.TELECTRONICA.telectro.local>
2016-05-04 10:30:44 -0300

    adding savings and updating logic

--- wt-rating.js
+++ wt-rating.js
@@ -1,19 +1,42 @@
 //Need to learn  JS >D
 var mongo = require('mongodb').MongoClient;
 var COLLECTION = 'items';
+
+
 var getNewAverage = function(quantity,currAvg,newPoint){
-  console.log('Getting new average');
-  return currAvg + ((newPoint-currAvg)/quantity);
+
+  var fCurrAvg = parseFloat(currAvg);
+  var fNewPoint = parseFloat(newPoint);
+  var fQuantity = parseFloat(quantity);
+
+  var result =fCurrAvg + ((fNewPoint-fCurrAvg)/fQuantity);
+
+  return result;
+
 }
 
 var findItem = function(db,item,cb){
-  console.log(item);
+
   db.collection(COLLECTION)
     .findOne(item,function(err,doc){
       if(err) throw err;
 
       cb(null,doc);
     });
+}
+
+
+var updateAverage = function(db,findedDoc,newPoint,cb){
+  var query = {link:findedDoc.link};
+
+  var newFields ={link:findedDoc.link,
+                  avgPoints:getNewAverage(findedDoc.quantity+1,findedDoc.avgPoints,newPoint),
+                  quantity:findedDoc.quantity+1};
+
+  db.collection(COLLECTION).findAndModify(query,[],newFields,{new:true},function(err,docUpdated){
+    if (err) throw err;
+    cb(null,docUpdated);
+  });
 }
 
 var saveAvgToCollection = function(db,docQuery,newPoint,cb){
@@ -24,38 +47,58 @@ var saveAvgToCollection = function(db,docQuery,newPoint,cb){
         //Extend properties on the new item
         docQuery.avgPoints=newPoint;
         docQuery.quantity=1; //It can probably be setted automatically by the mongo engine.
-        console.log(docQuery);
+
         db.collection(COLLECTION).insert(docQuery,function(err,res){
           if(err) throw err;
-          cb(null,res);
+          cb(null,res.ops[0]);
           return;
         });
       }else{          //Update
-        
+          updateAverage(db,findedDoc,newPoint,function(err,updated){
+            if(err) throw err;
+            cb(null,updated.value);
+          })
       }
    });
-
-
 }
 
+
+var findAllItems = function(db,cb){
+  db.collection(COLLECTION).find().toArray(function(err,documents){
+    if(err) throw err;
+
+    cb(null,documents);
+  });
+}
+
+//Auth0 main callback --> Execution is wrapped by Auth0 between try/Catch so you can throw errors.
 module.exports = function(context,callback){
-  try {
     var link = context.data.link;
     var points = context.data.points;
     var colQuery = {link:context.data.link};
 
     mongo.connect(context.data.MONGODB, function(err,db){
       if(err) return callback(err);
-      saveAvgToCollection(db,colQuery,points,function(err,itemSaved){
-        if (err) throw err;
-        console.log('FROM MAIN CALLBACK');
-        callback(null,itemSaved);
-      });
+      
+      if (!link) { //If u don't send any link, we'll send you the entire list?
+        findAllItems(db,function(err,totalList){
+          if (err) return callback(err);
 
+          callback(null,totalList);
+          return;
+        });
+      }else {
+        if (parseFloat(points)==points) { //Only accept NUMBERS
+          saveAvgToCollection(db,colQuery,points,function(err,itemSaved){
+            if (err) throw err;
 
-      });
-  } catch (e) {
-    callback('Exiting with error: '+e,null);
-  }
+            callback(null,itemSaved);
+          });
+
+        }else {
+          throw 'Invalid Point';
+        }
+      }
+    });
 
 }
